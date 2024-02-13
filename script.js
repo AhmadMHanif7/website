2c1c5ef742136328a7df8b8f40e7e6a03423a3d6
Ahmad <100248643+AhmadMHanif7@users.noreply.github.com>
2024-02-12 23:50:59 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("yes-btn").addEventListener("click", function() {
         document.getElementById("top-image").src = "images/IMG_9557.png";
-        document.getElementById("caption").innerText = "Good Choice, Inshallah next year we will celebrate in the same spot";
+        document.getElementById("caption").innerText = "Good Choice, Inshallah next year we will celebrate it together. Flowers and choclates for this year will be delivered in person";
         document.getElementById("yes-btn").style.display = "none";
         document.getElementById("no-btn").style.display = "none";
     });
